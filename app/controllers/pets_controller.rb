0d24dc515be187a3a6277f5a26a476da0650118f
Nina Hackenbroich <hackenbroich.nina@gmail.com>
2022-04-11 18:00:03 +0200

    lost and found complete

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,10 +1,11 @@
 class PetsController < ApplicationController
+  before_action :set_pet, only: [:show, :edit, :update, :destroy]
   def index
     @pets = Pet.all
   end
 
   def show
-    @pet = Pet.find(params[:id])
+    
   end
 
   def new
@@ -17,7 +18,27 @@ class PetsController < ApplicationController
     redirect_to pets_path
   end
 
+  def edit
+  end
+
+  def update
+    if @pet.update(pet_params)
+      redirect_to pet_path(@pet)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @pet.destroy
+    redirect_to pets_path
+  end
+
   private
+
+  def set_pet
+    @pet = Pet.find(params[:id])
+  end
 
   def pet_params
     params.require(:pet).permit(:name, :address, :species, :found_on)
--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,13 @@ Rails.application.routes.draw do
     # As a user I can add a pet I found
     get "pets/new", to: "pets#new"
     post "pets", to: "pets#create"
-      # As a user I can see details about one found pet (when and where it was found)
-    get "pets/:id", to: "pets#show"
-      # As a user I can update a pet
-      # As a user I can delete a pet
-
+    # As a user I can see details about one found pet (when and where it was found)
+    get "/pets/:id", to: "pets#show", as: :pet
+    # As a user I can update a pet
+    get "/pets/:id/edit", to: "pets#edit", as: :edit_pet
+    patch "/pets/:id", to: "pets#update"
+    # As a user I can delete a pet
+    delete "/pets/:id", to: "pets#destroy"
 
   # QUIZ
   # CRUD  HTTPverb   Controller Action     Active Record Method
